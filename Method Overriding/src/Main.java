74cd0a4cfc672680ae05bd667f0ab48f4e0bf22c
jsvillamayor <78101377+jsvillamayor@users.noreply.github.com>
2022-03-30 10:45:55 -0700

    Add files via upload

---
+++ Method Overriding/src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Dog dog = new Dog();
+		dog.speak();
+		Animal animal = new Animal();
+		animal.speak();
+	}
+
+}
